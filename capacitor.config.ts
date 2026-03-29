8ec6f8e5b37d75f166c8dfb166a9d119d3efda88
Anatole Couasnon <couasnon.anatole@gmail.com>
2026-03-29 15:18:08 +0200

    remove keyboard settings

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,11 +4,6 @@ const config: CapacitorConfig = {
 	appId: 'easy.gym.notebook',
 	appName: 'EasyGymNotebook',
 	webDir: 'dist/easy-gym-notebook/browser',
-	plugins: {
-		Keyboard: {
-			resize: 'none'
-		}
-	}
 };
 
 export default config;
